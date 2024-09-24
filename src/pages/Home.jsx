c001dec5550a4dac800f1e40706040e69e9cca89
Naman Pandey <naman.0816@gmail.com>
2024-09-24 16:28:51 +0530

    fifth commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=4d18e3d9aba346ca9c1c6bf836e56a55&response_type=code&redirect_uri=http://localhost:5173/user&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-read-private%20playlist-read-collaborative";
+  "https://accounts.spotify.com/authorize?client_id=4d18e3d9aba346ca9c1c6bf836e56a55&response_type=code&redirect_uri=https://musixapp.onrender.com/user&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20playlist-read-private%20playlist-read-collaborative";
 
 export default function HomePage() {
   return (
